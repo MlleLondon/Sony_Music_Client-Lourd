99cfc020b1fc3b60d1a4def128b9649a49aedd50
Garance_DUGIMONT <garancedugimont@gmail.com>
2023-01-20 14:44:00 +0100

    Ajout certaines choses

---
+++ sony_music/src/controleur/sonyMusic.java
@@ -0,0 +1,13 @@
+package controleur;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class sonyMusic {
+    private JTextField txtEmail= new JTextField();
+	private JPasswordField txtMdp= new JPasswordField();
+	private JButton btAnnuler= new JButton("Annuler");
+	private JButton btSeConnecter= new JButton("Connexion");
+	private JPanel panelCon=new JPanel();
+}
